10b613d8f37ede0efc26e13c67e1901bafec3be9
prasanganath <sandun.wed@gmail.com>
2019-01-22 18:37:10 +0530

    style

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -8,3 +8,11 @@ c.fillRect(100, 100, 100, 100);
 c.fillRect(400, 100, 100, 100);
 c.fillRect(300, 300, 100, 100);
 
+
+//Line
+c.beginPath();
+c.moveTo(50, 300);
+c.lineTo(300, 100);
+c.stroke();
+
+
