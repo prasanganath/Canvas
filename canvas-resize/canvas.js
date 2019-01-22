5b4feba7be9b0c153c4e05b8acfdf8842ffff7bf
prasanganath <sandun.wed@gmail.com>
2019-01-22 18:46:38 +0530

    stlye

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -4,7 +4,9 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+c.fillStyle = 'rgba(255, 0, 0,0.5)';
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0, 0, 0,0.5)';
 c.fillRect(400, 100, 100, 100);
 c.fillRect(300, 300, 100, 100);
 
