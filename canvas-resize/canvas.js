33cdb081196172640b67d1d5849b85f0e967dfd6
prasanganath <sandun.wed@gmail.com>
2019-01-22 23:20:23 +0530

    random circle

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -4,7 +4,7 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillStyle = 'rgba(255, 0, 0,0.5)';
+/*c.fillStyle = 'rgba(255, 0, 0,0.5)';
 c.fillRect(100, 100, 100, 100);
 c.fillStyle = 'rgba(0, 0, 0,0.5)';
 c.fillRect(400, 100, 100, 100);
@@ -18,6 +18,7 @@ c.lineTo(300, 100);
 c.lineTo(400, 300);
 c.strokeStyle = "#fa34a3";
 c.stroke();
+*/
 
 //Arc / Circle
 /*c.beginPath();
@@ -25,7 +26,7 @@ c.arc(300, 300, 30, 0, Math.PI * 2, false);
 c.strokeStyle = 'blue';
 c.stroke();*/
 
-for (var i = 0; i < 3; i++) {
+/*for (var i = 0; i < 1000; i++) {
 	var x = Math.random() * window.innerWidth;
 	var y = Math.random() * window.innerHeight;
 	c.beginPath();
@@ -33,4 +34,12 @@ for (var i = 0; i < 3; i++) {
 	c.strokeStyle = 'blue';
 	c.stroke();
 
+} */
+c.beginPath();
+c.arc(200, 200, 30, 0, Math.PI * 2, false);
+c.strokeStyle = 'blue';
+c.stroke();
+
+function animate(){
+	requestAnimationFrame(animate);
 }
