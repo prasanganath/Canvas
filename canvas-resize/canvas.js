20dd0700f928ec6e053c9f24385291c51e15355f
prasanganath <sandun.wed@gmail.com>
2019-01-23 07:48:00 +0530

    random circle

--- canvas-resize/canvas.js
+++ canvas-resize/canvas.js
@@ -35,42 +35,58 @@ c.stroke();*/
 	c.stroke();
 
 } */
-
-function Circle(x, y) {
-	this.x = x;
-	this.y = y;
-
-	this.draw = function() {
-		c.beginPath();
-		c.arc(x, y, radius, 0, Math.PI * 2, false);
-		c.strokeStyle = 'blue';
-		c.stroke();
-	}
-}
-var x = Math.random() * innerWidth;
+/*var x = Math.random() * innerWidth;
 var y = Math.random() * innerHeight;
 var dx = (Math.random() - 0.5) * 8;
 var dy = (Math.random() - 0.5) * 8;
-var radius = 30;
-function animate(){
+var radius = 30;*/
+
+function Circle(x, y, dx, dy, radius) {
+	this.x = x;
+	this.y = y;
+	this.dx = dx;
+	this.dy = dy;
+	this.radius = radius;
+
+	this.draw = function() {
+		c.beginPath();
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+		c.strokeStyle = 'blue';
+		c.stroke();
+	}
+	this.update = function() {
+		if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+			this.dx = -this.dx;
+		}
+		if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+			this.dy = -this.dy;
+ 
+		}
+
+		this.x +=this.dx;
+		this.y +=this.dy;
+
+		this.draw();
+		log.console("ssss");
+
+	}
+
+}
+/*var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random() - 0.5) * 8;
+var dy = (Math.random() - 0.5) * 8;
+var radius = 30; */
+
+var circle  = new Circle(200, 200, 3, 30);
+
+
+function animate() {
 	requestAnimationFrame(animate);
 	c.clearRect(0, 0, innerWidth, innerHeight);
-	c.beginPath();
-	c.arc(x, y, radius, 0, Math.PI * 2, false);
-	c.strokeStyle = 'blue';
-	c.stroke();
-
-	if (x + radius > innerWidth || x - radius < 0) {
-		dx = -dx;
-	}
-	if (y + radius > innerHeight || y - radius < 0) {
-		dy = -dy;
-
-	}
-
-	x +=dx;
-	y +=dy;
-
+ 
+	circle.update();
+	
 }
 
 animate();
